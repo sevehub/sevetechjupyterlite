a81c55a1ff251389c39cfdd23feaf707d3973d0e
@seve_py <https://tessarinseve.pythonanywhere.com/shared/request-form.html>
2025-11-03 14:50:20 +0000

    jupyter deck

--- packages/js-mathjs-extension/index.js
+++ packages/js-mathjs-extension/index.js
@@ -1,13 +1,14 @@
+
 export default {
   name: "js-mathjs-extension",
   async setup() {
-    await import("https://cdn.jsdelivr.net/npm/mathjs@11.8.0/lib/browser/math.js")
-      .then((module) => {
-        window.math = module;
-        console.log("Math.js loaded and available as window.math");
-      })
-      .catch((err) => {
-        console.error("Failed to load Math.js", err);
-      });
+    try {
+      const module = await import("https://cdn.jsdelivr.net/npm/mathjs@11.8.0/lib/browser/math.js");
+      window.math = module.create(module.all); // Explicitly create and assign math instance
+      console.log("Math.js loaded and available as window.math");
+    } catch (err) {
+      console.error("Failed to load Math.js", err);
+    }
   }
 };
+
